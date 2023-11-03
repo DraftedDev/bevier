c1e8f6ee79530e3bf8e375d00b6987b7da02a700
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2023-11-03 17:20:34 +0100

    Added paths and placeholder

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,14 @@
 use include_dir::{include_dir, Dir};
 use std::process::Command;
 
-pub static SIMPLE_3D: Dir = include_dir!("assets/simple_3d");
-pub static EMPTY_APP: Dir = include_dir!("assets/empty_app");
+pub static SIMPLE_3D: Dir = include_dir!("./assets/simple_3d");
+pub static EMPTY_APP: Dir = include_dir!("./assets/empty_app");
 
 pub static DEFAULT_CONFIG: &[u8] = include_bytes!("../assets/configs/default.toml");
 pub static SIZE_CONFIG: &[u8] = include_bytes!("../assets/configs/size.toml");
 pub static PERFORMANCE_CONFIG: &[u8] = include_bytes!("../assets/configs/performance.toml");
+
+pub static CARGO_TOML_PLACEHOLDER: &str = "_cargo_.toml";
 
 pub fn clear() {
     if cfg!(target_os = "windows") {
