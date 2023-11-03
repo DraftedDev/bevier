03e855fcccfe6de298cf257b318b4f071efec94a
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2023-11-03 18:27:06 +0100

    Update new.rs

--- src/new.rs
+++ src/new.rs
@@ -14,14 +14,14 @@ use log::info;
 pub fn init() -> InquireResult<()> {
     let name = Text::new("Project Name").prompt()?;
 
-    let template = Select::new("Project Type", vec!["Empty App", "Simple 3D"]).prompt()?;
+    let template = Select::new("Project Template", vec!["Empty App", "Simple 3D"]).prompt()?;
 
     let config = Select::new("Project Config", vec!["Default", "Size", "Performance"]).prompt()?;
 
     clear();
 
     info!("Project Name: {}", name);
-    info!("Project Type: {}", template);
+    info!("Project Template: {}", template);
     info!("Project Config: {}", config);
 
     if Confirm::new("Do you want to continue?").prompt()? {
