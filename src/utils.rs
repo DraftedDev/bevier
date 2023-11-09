e8593efaa9cca99e8bbb6d4b587ff6f3e5a075fe
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2023-11-09 17:39:12 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,18 @@
 use include_dir::{include_dir, Dir};
 use std::process::Command;
 
-pub static SIMPLE_3D: Dir = include_dir!("./assets/simple_3d");
-pub static EMPTY_APP: Dir = include_dir!("./assets/empty_app");
+pub const SIMPLE_3D: Dir = include_dir!("./assets/simple_3d");
+pub const SIMPLE_2D: Dir = include_dir!("./assets/simple_2d");
+pub const EMPTY_APP: Dir = include_dir!("./assets/empty_app");
 
-pub static DEFAULT_CONFIG: &[u8] = include_bytes!("../assets/configs/default.toml");
-pub static SIZE_CONFIG: &[u8] = include_bytes!("../assets/configs/size.toml");
-pub static PERFORMANCE_CONFIG: &[u8] = include_bytes!("../assets/configs/performance.toml");
+pub const DEFAULT_CONFIG: &[u8] = include_bytes!("../assets/configs/default.toml");
+pub const SIZE_CONFIG: &[u8] = include_bytes!("../assets/configs/size.toml");
+pub const PERFORMANCE_CONFIG: &[u8] = include_bytes!("../assets/configs/performance.toml");
 
-pub static CARGO_TOML_PLACEHOLDER: &str = "_cargo_.toml";
+pub const CARGO_TOML_PLACEHOLDER: &str = "_cargo_.toml";
+pub const CARGO_FEATURES_PLACEHOLDER: &str = "\"%FEATURES%\"";
 
+#[inline(always)]
 pub fn clear() {
     if cfg!(target_os = "windows") {
         Command::new("cmd")
@@ -27,6 +30,7 @@ pub fn clear() {
     };
 }
 
+#[inline(always)]
 pub fn exec(cmd: &str) {
     if cfg!(target_os = "windows") {
         Command::new("cmd")
